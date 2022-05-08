d78b524dbcc9c1793f2210bead8249739823ea9a
Planke85 <coa85@outlook.com>
2022-05-08 11:50:21 +0200

    Credo App

---
+++ AsyncImage/AsyncImage/AsyncImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncImageApp.swift
+//  AsyncImage
+//
+//  Created by Aleksandar Planic on 8.5.22.
+//
+
+import SwiftUI
+
+@main
+struct AsyncImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
