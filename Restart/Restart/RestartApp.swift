8ac1298d0cf593cf1952b62e5953d63379b04f1a
Planke85 <coa85@outlook.com>
2022-05-26 18:42:38 +0200

    Restart app update

---
+++ Restart/Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Aleksandar Planic on 8.5.22.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
